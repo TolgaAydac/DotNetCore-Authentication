64dff1a5182537e6798d36b99d5a137c8d23b1f2
TolgaAydac <>
2025-11-03 18:29:53 +0300

    Authentication düzenlemeleri ve eklentileri
    LoginRequest eklendi. register/login sorunsuz çalışıyor, veritabanı bağlantısı yapıldı, JWT token üretimi ve doğrulama eklendi.

--- Authentication/Authentication/Controllers/AuthControllers.cs
+++ Authentication/Authentication/Controllers/AuthControllers.cs
@@ -2,21 +2,27 @@ using Authentication.Models;
 using Authentication.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace Authentication.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly AuthDbContext _context;
+        private readonly IConfiguration _config;
 
 
-        public AuthController(AuthDbContext context)
+        public AuthController(AuthDbContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         //Register Endpoint
@@ -52,6 +58,33 @@ namespace Authentication.Controllers
             return Ok("Kayıt İşlemi Başarılı !");
         }
 
+        //Login Entpoint
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == request.Email);
+
+            if (user == null)
+                return Unauthorized("Kullanıcı bulunamadı!");
+
+            // Şifre kontrol
+            var hashedInput = HashPassword(request.Password);
+
+            if (user.Password != hashedInput)
+                return Unauthorized("Şifre yanlış!");
+
+            // Token oluşturma
+
+            var token = GenerateJwtToken(user);
+
+            return Ok(new { message = "Giriş başarılı", token });
+
+        }
+
+
+
         //Şifre Hashleme Methodu
         private string HashPassword(string password)
         {
@@ -61,6 +94,34 @@ namespace Authentication.Controllers
                 return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
             }
         }
+
+        private string GenerateJwtToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _config["JwtSettings:Secret"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+        new Claim("id", user.Id.ToString()),
+        new Claim("username", user.Username),
+        new Claim("email", user.Email)
+    };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JwtSettings:Issuer"],
+                audience: _config["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+
     }
 
 
